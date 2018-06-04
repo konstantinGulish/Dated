ab9fb9c48e2c4da2be7bcc705ec010603e0c3bd7
konstantinGulish <konstantin_gulish@yahoo.com>
2018-06-04 10:49:15 -0400

    updated logic

--- src/Dated.java
+++ src/Dated.java
@@ -4,7 +4,6 @@ import java.util.Date;
 import java.util.Calendar;
 import java.text.ParseException;
 import java.time.format.DateTimeFormatter;
-
 import java.util.Scanner;
 
 public class Dated {
@@ -20,29 +19,32 @@ public class Dated {
         DateTimeFormatter longFormat = DateTimeFormatter.ofPattern("dd MMMM yyyy");
 
         //Prompt user to enter the date:
-        String userD;
         System.out.println("Enter today's date: ");
-        userD = keyboard.next();
-        Date date1 = sdf.parse(userD);
         Date today = Calendar.getInstance().getTime();
-        System.out.println(today.toString());
+
+        LocalDate userDate;
+        String userD = keyboard.next();
+        userDate = LocalDate.parse(userD,dTF);
+        Date date1 = sdf.parse(userD);
+
+        String correctFormat = userDate.format(dMYF);
+        String userFormat = date1.toString();
 
         while (date1.compareTo(today) > 0)
         {
+
+            if (!userFormat.equals(correctFormat)) continue;
             System.out.println("Enter today's date: ");
             userD = keyboard.next();
             date1 = sdf.parse(userD);
         }
-        LocalDate userDate=null;
-        userDate = LocalDate.parse(userD,dTF);
 
         //This is how you parse a string with a date time formatter
-        System.out.println("This date is presented as day, Month,Year");
-        System.out.println(userDate.format(longFormat));
-        System.out.println("This date is presented as Month,day, Year");
-        System.out.println(userDate.format(shortMonthFormat));
-        System.out.println("This date is presented as Month (2 digit),Year");
-        System.out.println(userDate.format(dMYF));
-
+            System.out.println("This date is presented as day, Month,Year");
+            System.out.println(userDate.format(shortMonthFormat));
+            System.out.println("This date is presented as Month,day, Year");
+            System.out.println(userDate.format(shortMonthFormat));
+            System.out.println("This date is presented as Month (2 digit),Year");
+            System.out.println(userDate.format(dMYF));
     }
 }
